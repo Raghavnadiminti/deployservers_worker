9b5ae66c28dbfe13ea1d8ca168e696583063d163
Raghavnadiminti <raghavnadiminti@gmail.com>
2026-05-15 12:00:15 +0530

    axios errpr fix

--- controllers/dynamics.js
+++ controllers/dynamics.js
@@ -1,11 +1,31 @@
 const axios = require("axios");
 
+const axios = require("axios");
+
 async function getPublicIP() {
-    const res = await axios.get(
-        "http://169.254.169.254/latest/meta-data/public-ipv4"
+
+    const tokenRes = await axios.put(
+        "http://169.254.169.254/latest/api/token",
+        {},
+        {
+            headers: {
+                "X-aws-ec2-metadata-token-ttl-seconds": "21600"
+            }
+        }
     );
 
-    return res.data;
-}
+    const token = tokenRes.data;
 
+  
+    const ipRes = await axios.get(
+        "http://169.254.169.254/latest/meta-data/public-ipv4",
+        {
+            headers: {
+                "X-aws-ec2-metadata-token": token
+            }
+        }
+    );
+
+    return ipRes.data;
+}
 module.exports=getPublicIP
